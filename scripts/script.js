ebe6a69fd57217cd7720d6d2142f0a58b88c4c14
VLad <vlad15.prikhodko@gmail.com>
2022-07-20 14:56:10 +0300

    first commit

---
+++ scripts/script.js
@@ -0,0 +1,100 @@
+"use strict";
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// console.log('arr' + "- object");
+// console.log(4 + "5");
+
+
+// let incr = 10,
+// 	decr = 10;
+
+// 	// incr++;
+// 	// decr--;
+	
+// 	console.log(++incr);
+// 	console.log(--decr);
+
+
+// 	console.log(5%2);
+
+// 	console.log(2 + 2 * 2 != '6');
+
+// 	const isChecked = false,
+// 		isClose = false;
+
+// 		console.log(isChecked || !isClose);
+
+
+
+
+// const category = 'toys';
+
+// console.log(`https://someurl.com/${category}/5`);
+
+// const user = "ivan";
+// alert(`Привет, ${user}`);
+
+// alert('Hello');
+
+// const result = confirm("Are you here?");
+// console.log(result);
+
+// const answer = +prompt("Вам есть 18?", "");
+// console.log(answer + 5);
+
+// const answers = [];
+
+// answers[0] = prompt('Как ваше имя?', '');
+// answers[1] = prompt('Сколько вам лет?', '');
+// answers[2] = prompt('Где вы живете?', '');
+
+// console.log(typeof(answers));
+// console.log(typeof(null));
+
+
+
+// // a = 15;
+// // console.log(a);
+
+// let number = 5;
+// const leftBorderWidth = 1;
+
+
+// number = 10;
+// console.log(number);
+
+
+// // const obj = {
+// // 	a:50
+
+// // };
+
+// // obj.a = 10;
+
+// // console.log(leftBorderWidth);
+
+// // console.log(name);
+
+// // var name = 'Ivan';
+
+// // name = 'Alex';
+// {
+// 	var result = 50;
+// }
+// console.log(result);
